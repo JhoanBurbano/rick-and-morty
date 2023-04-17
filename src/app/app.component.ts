8e84e39ec7b06bd4b1f9549bbaeb22a5b1586f0f
jsburbano <jhoan@aplazo.mx>
2023-04-17 00:36:50 -0500

    add: loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rick-and-morty';
+  cargando= true;
+
+  constructor(private  router: Router) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.setLoader(500)
+      }
+    });
+  }
+
+  setLoader(interval: number = 1000){
+  this.cargando= true;
+  setTimeout(()=>{
+    this.cargando = false
+  }, interval)
+
+
+  }
 }
